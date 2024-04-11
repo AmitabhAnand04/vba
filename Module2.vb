4ba52f6bac9f39d7cfc86a963a948a27ab3e0119
AmitabhAnand04 <98702241+AmitabhAnand04@users.noreply.github.com>
2024-04-11 23:49:12 +0530

    Update TestModule2.vb

--- Module2.vb
+++ Module2.vb
@@ -5,4 +5,11 @@ Sub TestModule2()
     For i = 1 To 100
         Sheet1.Cells(i, j).Value = i
     Next i
+    
+    Dim j As Integer
+    For j = 1 To 2
+        For i = 1 To 100
+            Sheet1.Cells(i, j).Value = i
+        Next i
+    Next j
 End Sub

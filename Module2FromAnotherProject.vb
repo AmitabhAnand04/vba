998957bcd508c9908041219b1e9da1ec61d960bf
AmitabhAnand04 <98702241+AmitabhAnand04@users.noreply.github.com>
2024-04-12 10:02:13 +0530

    Updating Module

--- Module2FromAnotherProject.vb
+++ Module2FromAnotherProject.vb
@@ -2,10 +2,7 @@ Option Explicit
 
 Sub TestModule2()
     Dim i As Integer
-    Dim j As Integer
-    For j = 1 To 2
-        For i = 1 To 100
-            Sheet1.Cells(i, j).Value = i
-        Next i
-    Next j
+    For i = 1 To 100
+        Sheet1.Cells(i, j).Value = i
+    Next i
 End Sub
